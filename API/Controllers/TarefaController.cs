a96a9b97cfc5a38c28639e7b60a9e11f03951f9c
felipec0elho <felipe.soares.novenove@gmail.com>
2023-11-24 09:48:00 -0300

    aa

---
+++ API/Controllers/TarefaController.cs
@@ -0,0 +1,116 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using API.Data;
+using API.Models;
+
+namespace API.Controllers;
+
+[Route("api/tarefa")]
+[ApiController]
+public class TarefaController : ControllerBase
+{
+    private readonly AppDataContext _context;
+
+    public TarefaController(AppDataContext context) =>
+        _context = context;
+
+    // GET: api/tarefa/listar
+    [HttpGet]
+    [Route("listar")]
+    public IActionResult Listar()
+    {
+        try
+        {
+            List<Tarefa> tarefas = _context.Tarefas.Include(x => x.Categoria).ToList();
+            return Ok(tarefas);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    // POST: api/tarefa/cadastrar
+    [HttpPost]
+    [Route("cadastrar")]
+    public IActionResult Cadastrar([FromBody] Tarefa tarefa)
+    {
+        try
+        {
+            Categoria? categoria = _context.Categorias.Find(tarefa.CategoriaId);
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+            tarefa.Categoria = categoria;
+            _context.Tarefas.Add(tarefa);
+            _context.SaveChanges();
+            return Created("", tarefa);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    [HttpPut]
+    [Route("alterar")]
+    public IActionResult Alterar([FromBody] Tarefa tarefa)
+    {
+        try
+        {
+            var tarefaCadastrada = _context.Tarefas.FirstOrDefault(x => x.TarefaId == tarefa.TarefaId);
+
+            if (tarefaCadastrada != null)
+            {
+                if (tarefaCadastrada.Status == string.Empty)
+                {
+                    tarefaCadastrada.Status = "Em andamento";
+                }
+                else if (tarefaCadastrada.Status == "Em andamento")
+                {
+                    tarefaCadastrada.Status = "Concluida";
+                }
+                _context.Tarefas.Update(tarefaCadastrada);
+                _context.SaveChanges();
+                return Ok();
+            }
+            return NotFound();
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+            throw;
+        }
+    }
+
+    [HttpGet]
+    [Route("naoconcluidas")]
+    public IActionResult ListarNaoConcluidas()
+    {
+        try
+        {
+            var tarefas = _context.Tarefas.Where(x => x.Status == "Em andamento");
+            return Ok(tarefas);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    [HttpGet]
+    [Route("concluidas")]
+    public IActionResult ListarConcluidas()
+    {
+        try
+        {
+            var tarefas = _context.Tarefas.Where(x => x.Status == "Concluida");
+            return Ok(tarefas);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+}
---
+++ API/Migrations/20231124115346_Initial.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categorias",
+                columns: table => new
+                {
+                    CategoriaId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", nullable: true),
+                    CriadoEm = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categorias", x => x.CategoriaId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tarefas",
+                columns: table => new
+                {
+                    TarefaId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Titulo = table.Column<string>(type: "TEXT", nullable: true),
+                    Descricao = table.Column<string>(type: "TEXT", nullable: true),
+                    CriadoEm = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    CategoriaId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Status = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tarefas", x => x.TarefaId);
+                    table.ForeignKey(
+                        name: "FK_Tarefas_Categorias_CategoriaId",
+                        column: x => x.CategoriaId,
+                        principalTable: "Categorias",
+                        principalColumn: "CategoriaId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categorias",
+                columns: new[] { "CategoriaId", "CriadoEm", "Nome" },
+                values: new object[] { 1, new DateTime(2023, 11, 25, 8, 53, 46, 632, DateTimeKind.Local).AddTicks(9519), "Trabalho" });
+
+            migrationBuilder.InsertData(
+                table: "Categorias",
+                columns: new[] { "CategoriaId", "CriadoEm", "Nome" },
+                values: new object[] { 2, new DateTime(2023, 11, 26, 8, 53, 46, 632, DateTimeKind.Local).AddTicks(9523), "Estudos" });
+
+            migrationBuilder.InsertData(
+                table: "Categorias",
+                columns: new[] { "CategoriaId", "CriadoEm", "Nome" },
+                values: new object[] { 3, new DateTime(2023, 11, 27, 8, 53, 46, 632, DateTimeKind.Local).AddTicks(9524), "Lazer" });
+
+            migrationBuilder.InsertData(
+                table: "Tarefas",
+                columns: new[] { "TarefaId", "CategoriaId", "CriadoEm", "Descricao", "Status", "Titulo" },
+                values: new object[] { 1, 1, new DateTime(2023, 12, 1, 8, 53, 46, 632, DateTimeKind.Local).AddTicks(9594), "Terminar relatório para reunião", "", "Concluir relatório" });
+
+            migrationBuilder.InsertData(
+                table: "Tarefas",
+                columns: new[] { "TarefaId", "CategoriaId", "CriadoEm", "Descricao", "Status", "Titulo" },
+                values: new object[] { 2, 2, new DateTime(2023, 11, 27, 8, 53, 46, 632, DateTimeKind.Local).AddTicks(9596), "Preparar-se para a aula de Angular", "", "Estudar Angular" });
+
+            migrationBuilder.InsertData(
+                table: "Tarefas",
+                columns: new[] { "TarefaId", "CategoriaId", "CriadoEm", "Descricao", "Status", "Titulo" },
+                values: new object[] { 3, 3, new DateTime(2023, 12, 8, 8, 53, 46, 632, DateTimeKind.Local).AddTicks(9598), "Dar um passeio relaxante no parque", "", "Passeio no parque" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tarefas_CategoriaId",
+                table: "Tarefas",
+                column: "CategoriaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tarefas");
+
+            migrationBuilder.DropTable(
+                name: "Categorias");
+        }
+    }
+}
